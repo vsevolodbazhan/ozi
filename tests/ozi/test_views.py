a0f3670ea0576881362a6c6723d49003c08f4bee
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-02 09:18:18 +1000

    Add tests for `subscribe-client` handler

--- tests/ozi/test_views.py
+++ tests/ozi/test_views.py
@@ -13,6 +13,11 @@ def _client(db, faker):
     return Client.objects.create(bot=faker.pystr(), chat=faker.pystr())
 
 
+@pytest.fixture
+def mailing(db, user, faker):
+    return Mailing.objects.create(user=user, name=faker.pystr())
+
+
 def test_list_mailings_with_no_mailings(client, config):
     url = reverse("list-mailings")
     response = client.post(url, config, content_type="application/json")
@@ -63,7 +68,6 @@ def test_list_subscriptions(client, config, user, _client, faker):
 def test_list_subscriptions_creates_client(client, config, faker):
     url = reverse("list-subscriptions")
     data = {"bot_id": faker.pystr(), "chat_id": faker.pystr(), **config}
-
     response = client.post(url, data, content_type="application/json")
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
@@ -76,3 +80,72 @@ def test_list_subscriptions_requires_authentication(client):
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
     assert set(response.data.keys()) == set(["detail"])
+
+
+def test_subscribe_client(client, config, _client, mailing):
+    url = reverse("subscribe-client")
+    data = {
+        "chat_id": _client.chat,
+        "bot_id": _client.bot,
+        "mailing_id": mailing.id,
+        **config,
+    }
+    response = client.post(url, data, content_type="application/json")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert _client.subscriptions.count() == 1
+
+
+def test_subscribe_client_creates_client(client, config, mailing, faker):
+    url = reverse("subscribe-client")
+    data = {
+        "chat_id": faker.pystr(),
+        "bot_id": faker.pystr(),
+        "mailing_id": mailing.id,
+        **config,
+    }
+    response = client.post(url, data, content_type="application/json")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Client.objects.count() == 1
+
+
+def test_subscribe_client_requires_mailing(client, config, _client, faker):
+    url = reverse("subscribe-client")
+    data = {
+        "chat_id": _client.chat,
+        "bot_id": _client.bot,
+        "mailing_id": faker.pyint(),
+        **config,
+    }
+    response = client.post(url, data, content_type="application/json")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert set(response.data.keys()) == set(["detail"])
+
+
+def test_subscribe_client_can_conflict(client, config, _client, mailing):
+    _client.subscriptions.add(mailing)
+    url = reverse("subscribe-client")
+    data = {
+        "chat_id": _client.chat,
+        "bot_id": _client.bot,
+        "mailing_id": mailing.id,
+        **config,
+    }
+    response = client.post(url, data, content_type="application/json")
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert set(response.data.keys()) == set(["detail"])
+
+
+def test_subscribe_client_requires_authentication(client, _client, mailing):
+    url = reverse("subscribe-client")
+    data = {
+        "chat_id": _client.chat,
+        "bot_id": _client.bot,
+        "mailing_id": mailing.id,
+    }
+    response = client.post(url, data, content_type="application/json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
