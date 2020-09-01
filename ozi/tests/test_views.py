fc4ccb2ed67dbc99d245d50a41d5637a94548a29
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-01 15:10:08 +1000

    Add tests for `list-subscriptions` handler

--- ozi/tests/test_views.py
+++ ozi/tests/test_views.py
@@ -6,18 +6,24 @@ from rest_framework.test import APITestCase
 
 from accounts.models import Token
 
-from ..models import Mailing
+from ..models import Client, Mailing
 from ..utilities import build_config
 
 User = get_user_model()
 fake = Faker()
 
 
+def set_up():
+    user = User.objects.create(email=fake.email(), password=fake.password())
+    token = Token.objects.get(user=user)
+    config = build_config({"token": str(token)})
+
+    return user, config
+
+
 class TestMailing(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(email=fake.email(), password=fake.password())
-        self.token = Token.objects.get(user=self.user)
-        self.config = build_config({"token": str(self.token)})
+        self.user, self.config = set_up()
 
     def test_list_mailings_with_no_mailings(self):
         url = reverse("list-mailings")
@@ -35,8 +41,40 @@ class TestMailing(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertCountEqual(response.data, ["mailings"])
 
-    def test_requires_authentication(self):
+    def test_list_mailings_requires_authentication(self):
         url = reverse("list-mailings")
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertCountEqual(response.data, ["detail"])
+
+
+class TestClient(APITestCase):
+    def setUp(self):
+        self.user, self.config = set_up()
+        self._client = Client.objects.create(bot=fake.pystr(), chat=fake.pystr())
+
+    def test_list_subscriptions_with_no_subscriptions(self):
+        url = reverse("list-subscriptions")
+        data = {"bot_id": self._client.bot, "chat_id": self._client.chat, **self.config}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_subscriptions(self):
+        for _ in range(5):
+            mailing = Mailing.objects.create(user=self.user, name=fake.pystr())
+            self._client.subscriptions.add(mailing)
+
+        url = reverse("list-subscriptions")
+        data = {"bot_id": self._client.bot, "chat_id": self._client.chat, **self.config}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertCountEqual(response.data, ["subscriptions"])
+
+    def test_list_subscriptions_requires_authentication(self):
+        url = reverse("list-subscriptions")
         response = self.client.post(url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
