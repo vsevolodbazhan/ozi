1a3db819a1d31e3a89c83a484c747d5857f38277
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-06 17:22:19 +1000

    Temporaly remove `plan-update` handler

--- ozi/urls.py
+++ ozi/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path("subscribe", views.subscribe_client, name="subscribe-client"),
     path("unsubscribe", views.unsubscribe_client, name="unsubscribe-client"),
     path("find-mailing", views.find_mailing, name="find-mailing"),
-    path("plan-update", views.plan_update, name="plan-update"),
     path(
         "schedule-update",
         views.schedule_update_for_client,
